6a42f036d59649928b360b629c1ef984d6adbff2
maboxuanlive <60963442+maboxuanlive@users.noreply.github.com>
2020-12-29 15:11:40 +0800

    Add files via upload

---
+++ src/bookmanage/model/Book.java
@@ -0,0 +1,47 @@
+package bookmanage.model;
+/**
+ * @Description 图书实体类
+ * @author com.javayihao.top
+ */
+public class Book {
+	private String id;//图书编号
+	private String name;//图书名称
+	private int num;//数量
+	private float price;//价格
+	//无参构造方法
+	public Book() {
+		super();
+	}
+	//全参构造方法
+	public Book(String id, String name, int num, float price) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.num = num;
+		this.price = price;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+}
